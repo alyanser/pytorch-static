99fce5cbc21ffec8ba1408f817fcc406882a23ae
Aly Anser <alyystaz@gmail.com>
2022-08-28 16:29:11 +0500

    upd windows script

--- windows-build.bat
+++ windows-build.bat
@@ -2,10 +2,39 @@
 
 set GENERATOR=Ninja
 set PYTORCH_DIR=pytorch
-set INSTALL_PREFIX=%cd%\example\pytorch
+set INSTALL_PREFIX=%cd%\%PYTORCH_DIR%\build\install
+
+set args =-G%GENERATOR%
+set args =%args% -DCMAKE_BUILD_TYPE=MinSizeRel
+set args =%args% -DBUILD_SHARED_LIBS=OFF
+set args =%args% -DCAFFE2_USE_MSVC_STATIC_RUNTIME=ON
+set args =%args% -DMSVC_Z7_OVERRIDE=ON
+
+set args =%args% -DBUILD_CAFFE2=OFF
+set args =%args% -DBUILD_PYTHON=OFF
+set args =%args% -DBUILD_TEST=OFF
+set args =%args% -DBUILD_LAZY_TS_BACKEND=OFF
+
+set args =%args% -DUSE_FBGEMM=ON
+set args =%args% -DUSE_NNPACK=ON
+set args =%args% -DBUILD_CUSTOM_PROTOBUF=ON
+
+set args =%args% -DUSE_NCCL=OFF
+set args =%args% -DUSE_CUDA=OFF
+set args =%args% -DUSE_MKLDNN=OFF
+set args =%args% -DUSE_GLOO=OFF
+set args =%args% -DUSE_KINETO=OFF
+set args =%args% -DUSE_NUMPY=OFF
+set args =%args% -DUSE_OPENMP=OFF
+set args =%args% -DUSE_XNNPACK=OFF
+set args =%args% -DUSE_DISTRIBUTED=OFF
+set args =%args% -DUSE_MPI=OFF
+set args =%args% -DUSE_TENSORPIPE=OFF
+set args =%args% -DUSE_PYTORCH_QNNPACK=OFF
+set args =%args% -DUSE_QNNPACK=OFF
+set args =%args% -DUSE_FFTW=OFF
+
 cd %PYTORCH_DIR%
-
-cmake . -B build -G%GENERATOR% -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DBUILD_CAFFE2_MOBILE=OFF -DCAFFE2_USE_MSVC_STATIC_RUNTIME=ON -DMSVC_Z7_OVERRIDE=ON -DUSE_CUDA=OFF -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF -DUSE_MKLDDN=OFF -DBUILD_CAFFE2=OFF -DUSE_GLOO=OFF -DUSE_KINETO=OFF -DUSE_FFTW=OFF -DUSE_NUMPY=OFF -DUSE_NNPACK=ON -DUSE_OPENMP=OFF -DUSE_XNNPACK=OFF -DUSE_DISTRIBUTED=OFF -DUSE_MPI=OFF -DUSE_TENSORPIPE=OFF -DBUILD_LAZY_TS_BACKEND=OFF -DUSE_PYTORCH_QNNPACK=OFF -DUSE_FBGEMM=ON -DUSE_QNNPACK=OFF -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%
-
+cmake . -B build %args%
 cd build
 cmake --build . --target install
